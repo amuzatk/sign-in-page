8cc5101927349b27ba34450b7d9aaa2c46cea174
Kazmatics <kazeem.amuzat@centrifugegroup.com>
2023-02-18 22:37:55 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
 import './App.css';
 
-function App() {
+function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [role, setRole] = useState('employee');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert('Coming soon');
+    console.log(email, role, password);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <form onSubmit={handleSubmit}>
+
+<FloatingLabel
+        controlId="floatingInput"
+        label="Email address"
+        className="mb-3"
+      >
+        <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)} required placeholder="name@example.com" />
+      </FloatingLabel>
+
+      <FloatingLabel className='mb-3' controlId="floatingPassword" label="Password">
+        <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} required placeholder="Password" />
+      </FloatingLabel>
+     
+        <Form.Select value={role} onChange={(e) => setRole(e.target.value)} aria-label="Default select example">
+      <option>select role</option>
+      <option value="admin">admin</option>
+      <option value="employees">employees</option>
+    </Form.Select>
+  
+      <br />
+      <Button type="submit" variant="primary">Sign In</Button>
+    </form>
     </div>
   );
 }
 
-export default App;
+export default SignIn;
